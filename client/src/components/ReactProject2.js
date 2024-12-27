baf70887d6f83d679719a69f1a305996ff8bfa3d
Clay Mangum <clay@trinitasaccounting.com>
2024-12-27 11:35:53 -0500

    updating and submitting form properly

--- client/src/components/ReactProject2.js
+++ client/src/components/ReactProject2.js
@@ -10,27 +10,55 @@ function ReactProject2() {
         age: ""
     })
 
+    const [dataVisible, setDataVisible] = useState(false)
+
 
     // function to handle form update
+    const handleChange = (event) => {
+        setFormData({ ...formData, [event.target.name]: event.target.value })
+        setDataVisible(false)
+    }
+
+    // console.log(formData)
 
 
     // function to print the form data onsubmit
+    // let dataVisible = false
+    // const showData = (dataVisible) => {
+    //     dataVisible = !dataVisible
+    // }
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+        setDataVisible(dataVisible => !dataVisible)
+        setFormData({
+            username: "",
+            fullname: "",
+            age: ""
+        })
+        // console.log("Tomatoes")
+    }
+    console.log(dataVisible)
 
 
     return (
         <>
             <h1>Test React Project 2</h1>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>Username:
-                    <input type="text" value={formData.username} />
+                    <input type="text" value={formData.username} onChange={handleChange} name="username" />
                 </label>
                 <label>Fullname:
-                    <input type="text" value={formData.fullname} />
+                    <input type="text" value={formData.fullname} onChange={handleChange} name="fullname" />
                 </label>
                 <label>Age:
-                    <input type="text" value={formData.age} />
+                    <input type="text" value={formData.age} onChange={handleChange} name="age" />
                 </label>
+                <button type="submit">Submit</button>
             </form>
+            <div>
+                {dataVisible ? <h1>Request Sent to DB with below request data</h1> : <></>}
+            </div>
         </>
     )
 }
