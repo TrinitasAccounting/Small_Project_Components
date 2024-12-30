3f5eb44afbfb0e8652fca0f4c9a8dcc36fe4116f
Clay Mangum <clay@trinitasaccounting.com>
2024-12-30 10:04:15 -0500

    fetching data from open source API and changing fetch index everytime next post is clicked

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -15,9 +15,6 @@ function App() {
     setCities(cities.filter((city) => city !== cityToDelete))
   }
 
-
-
-
   // Map through the cities array and output each in list format
   let citiesToDisplay = cities.map((city) => (
     <>
@@ -25,6 +22,39 @@ function App() {
       <button onClick={() => deleteCity(city)}>X</button>
     </>
   ))
+
+
+  // Fetch data component
+
+
+  const [dataFetched, setDataFetched] = useState("")
+  const [index, setIndex] = useState(1)
+
+  // Fetch function from the url provided
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${index}`)
+      .then(res => {
+        if (res.ok) {
+          res.json().then(data => setDataFetched(data))
+        }
+      })
+  }, [index])
+
+  console.log(dataFetched);
+
+
+  // function to increment the index
+  const handleClickOfNextPost = () => {
+    if (index < 6) {
+      setIndex(() => index + 1)
+    }
+    else {
+      setIndex(1)
+    }
+    // setIndex(() => index + 1)
+  }
+
+  console.log(index)
 
 
 
@@ -39,7 +69,11 @@ function App() {
         {citiesToDisplay}
       </ul> */}
       {/* <TimerComponent /> */}
-      <ButtonCounter />
+      {/* <ButtonCounter /> */}
+      <h2>id: {dataFetched.id}</h2>
+      <h3>title: {dataFetched.title}</h3>
+      <h4>body: {dataFetched.body}</h4>
+      <button onClick={handleClickOfNextPost} style={{ backgroundColor: 'purple', color: 'white' }}>Next Post</button>
     </>
   )
 }
