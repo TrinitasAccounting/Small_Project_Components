877588e47926d18670c8ec8647a8921b95571a2e
Clay Mangum <clay@trinitasaccounting.com>
2025-01-08 16:16:17 -0500

    drag and drop is fully functional and added to navbar

--- client/src/components/Navbar/SortableListPage.js
+++ client/src/components/Navbar/SortableListPage.js
@@ -7,59 +7,50 @@ import React, { useRef, useState } from 'react';
 const SortableListPage = () => {
 
     const [people, setPeople] = useState([
-        { id: '1', name: 'John', content: 'Item 1' },
-        { id: '2', name: 'Clayton', content: 'Item 2' },
-        { id: '3', name: 'Mangum', content: 'Item 3' },
+        { id: '1', name: 'Tim Henry ', content: 'Item 1' },
+        { id: '2', name: 'Clayton Bigsby', content: 'Item 2' },
+        { id: '3', name: 'Dylan Humphrey', content: 'Item 3' },
+        { id: '4', name: 'Sarah Johnson', content: 'Item 4' },
+        { id: '5', name: 'Carley Llyodd', content: 'Item 5' },
     ]);
 
-    // const handleOnDragEnd = (result) => {
-    //     if (!result.destination) return;
 
-    //     const newItems = Array.from(items)
-    //     const [reorderedItem] = newItems.splice(result.source.index, 1);
-    //     newItems.splice(result.destination.index, 0, reorderedItem)
 
-    //     setItems(newItems);
+    const dragPerson = useRef(0)
+    const draggedOverPerson = useRef(0)
 
-    // }
+    function handleSort() {
+        const peopleClone = [...people]
+        const temp = peopleClone[dragPerson.current]
+        peopleClone[dragPerson.current] = peopleClone[draggedOverPerson.current]
+        peopleClone[draggedOverPerson.current] = temp
+
+        setPeople(peopleClone)
+    }
 
 
 
 
     return (
-        // <DragDropContext onDragEnd={handleOnDragEnd}>
-        //     <Droppable droppableId="list">
-        //         {(provided) => (
-        //             <ul {...provided.droppableProps} ref={provided.innerRef}>
-        //                 {items.map((item, index) => (
-        //                     <Draggable key={item.id} draggableId={item.id} index={index}>
-        //                         {(provided) => (
-        //                             <li
-        //                                 {...provided.draggableProps}
-        //                                 {...provided.dragHandleProps}
-        //                                 ref={provided.innerRef}
-        //                             >
-        //                                 {item.content}
-        //                             </li>
-        //                         )}
-        //                     </Draggable>
-        //                 ))}
-        //                 {provided.placeholder}
-        //             </ul>
-        //         )}
-        //     </Droppable>
-        // </DragDropContext>
-        // );
 
-        <>
-            <h1>List</h1>
-            {people.map((person) => {
+        <div className="center" style={{ backgroundColor: "gray" }}>
+            <h1 style={{ color: "white" }}>List</h1>
+            {people.map((person, index) => {
                 return (
-                    <ul>{person.name}</ul>
+                    <div style={{ border: "solid", width: "200px", marginTop: "10px", backgroundColor: "pink" }}
+                        draggable
+                        onDragStart={() => (dragPerson.current = index)}
+                        onDragEnter={() => (draggedOverPerson.current = index)}
+                        onDragEnd={handleSort}
+                        onDragOver={(event) => event.preventDefault()}
+                    >
+                        <p style={{ marginLeft: "10px", padding: "1px", display: "inline-block" }}>{person.name} </p>
+                        <p style={{ display: "inline-block", position: 'absolute', right: "60px" }}>&#8645;</p>
+                    </div>
                 )
             })}
 
-        </>
+        </div>
     )
 
 }
